eb5d196947960c56034feb2cea32f63eb0f94796
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-08-22 16:38:45 -0700

    Exercise 1.1 finished

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,46 +2,55 @@
 const Header = (props) => {
   return(
     <>
+      <h1>{props.course}</h1>
     </>
   )
 }
 
 const Content = (props) => {
+  //Idea taken from React's documentation at https://react.dev/learn
+  //I thought there must be a way to print this
+  //Without writing the component out three times.
+  const listParts = props.parts.map(part =>
+    <h3><i>{part.name}</i> - {part.exercises} exercises</h3>
+    );
   return (
     <>
+      {listParts}
     </>
   )
 }
 
 const Total = (props) => {
+  //I thought there might also be a way to calculate it
+  //So I checked the Array documentation, going from map()
+  //To looking for a "forEach" function like native JS has.
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+  var exerciseTotal = 0;
+  props.parts.forEach((part) => {
+    exerciseTotal += part.exercises;
+  });
   return (
     <>
+    <h3>Number of exercises: <b>{exerciseTotal}</b></h3>
     </>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    { part:1, name:"Fundamentals of React", exercises:10 },
+    { part:2, name:"Using props to pass data", exercises:7 },
+    { part:3, name:"State of a component", exercises:14 }
+  ]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
