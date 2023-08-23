ea7a61a31db5bf7d05fe7cfde1694743b4b30711
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-08-22 18:50:51 -0700

    Exercise 1.3 Done. Created Content2 and Part2 for it

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -31,6 +31,26 @@ const Content = (props) => {
   )
 }
 
+const Part2 = (props) => {
+  return (
+    <>
+      <p>{props.part.name} - {props.part.exercises}</p>
+    </>
+  )
+}
+
+const Content2 = (props) => {
+  //For exercise 1.3
+  //Input = "part" object {name:str, exercises:int}
+  return (
+    <>
+    <Part2 part={props.part1} />
+    <Part2 part={props.part2} />
+    <Part2 part={props.part3} />
+    </>
+  )
+}
+
 const Total = (props) => {
   //I thought there might also be a way to calculate it
   //So I checked the Array documentation, going from map()
@@ -55,11 +75,24 @@ const App = () => {
     { part:2, name:"Using props to pass data", exercises:7 },
     { part:3, name:"State of a component", exercises:14 }
   ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  //<Content parts={parts} />
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
+      <Content2 part1={part1} part2={part2} part3={part3} />
       <Total parts={parts} />
     </div>
   )
