bcf2fd9cab302dbaac095fad7848d932e1a329ca
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-08-27 22:37:59 -0700

    a more complex state object demo

--- part1/part1c/src/App.jsx
+++ part1/part1c/src/App.jsx
@@ -12,24 +12,61 @@ const Hello = ({name, age}) => {
   )
 }
 
-const Display = (props) => {
-  return(
-    <div>{props.counter}</div>
-  )
-}
-const Button = (props) => {
+const Display = ({ counter }) => <div>{counter}</div>
+const Button = ({ handleClick, text}) => {
   return (
-    <button onClick={props.handleClick}>
-        {props.text}
+    <button onClick={handleClick}>
+        {text}
     </button>
   )
 }
 
 const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = { 
+      left: clicks.left + 1, 
+      right: clicks.right 
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = { 
+      left: clicks.left, 
+      right: clicks.right + 1 
+    }
+    setClicks(newClicks)
+  }
+
+  return (
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
+  )
+}
+
+const App1 = () => {
   const [ counter, setCounter ] = useState(0)
-  const increaseByOne = () => setCounter(counter+1);
-  const setToZero = () => setCounter(0);
-  const decreaseByOne = () => setCounter(counter-1);
+  console.log('rendering with counter value', counter)
+  const increaseByOne = () => {
+    console.log("increasing, value before ", counter)
+    setCounter(counter+1);
+  }
+  const setToZero = () => {
+    console.log("resetting to zero, value before ", counter)
+    setCounter(0);
+  }
+  const decreaseByOne = () => {
+    console.log("decreasing, value before ", counter)
+    setCounter(counter-1);
+  }
   return (
     <>
       <Display counter={counter}/>
