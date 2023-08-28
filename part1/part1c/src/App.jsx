db37f07b26c77410c767176860577f37dddfdd02
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-08-28 16:37:42 -0700

    state is updated async, at some point, demo

--- part1/part1c/src/App.jsx
+++ part1/part1c/src/App.jsx
@@ -22,30 +22,35 @@ const Button = ({ handleClick, text}) => {
 }
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
-    setClicks({ 
-      ...clicks,
-      left: clicks.left + 1, 
-    })
+    setAll(allClicks.concat('L'))
+    console.log("left before ", left)
+    setLeft(left + 1)
+    console.log("left after ", left)
+    setTotal(left + right)
   }
 
   const handleRightClick = () => {
-    setClicks({ 
-      ...clicks,
-      right: clicks.right + 1 
-    })
+    setAll(allClicks.concat('R'))
+    console.log("right before ", left)
+    setRight(right + 1)
+    console.log("left after ", left)
+    setTotal(left + right)
   }
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
