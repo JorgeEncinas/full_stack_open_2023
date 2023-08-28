c37ea2483ed024de4979004bde69c300d19baa09
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-08-27 21:42:40 -0700

    state demo

--- part1/part1c/src/App.jsx
+++ part1/part1c/src/App.jsx
@@ -12,20 +12,30 @@ const Hello = ({name, age}) => {
   )
 }
 
+const Display = (props) => {
+  return(
+    <div>{props.counter}</div>
+  )
+}
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+        {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
-  const handleClick = () => {
-    setCounter(counter+1);
-  }
+  const increaseByOne = () => setCounter(counter+1);
+  const setToZero = () => setCounter(0);
+  const decreaseByOne = () => setCounter(counter-1);
   return (
     <>
-      <div>{counter}</div>
-      <button onClick={handleClick}>
-        Add 1
-      </button>
-      <button onClick={() => setCounter(0)}>
-        Reset counter
-      </button>
+      <Display counter={counter}/>
+      <Button handleClick={increaseByOne} text="Add 1" />
+      <Button handleClick={setToZero} text="Reset to 0"/>
+      <Button handleClick={decreaseByOne} text="Subtract 1" />
     </>
   )
 }
