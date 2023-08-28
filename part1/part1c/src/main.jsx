b13ae3fe8847d4254e9bcd55d1813438e3a0ae7a
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-08-27 18:56:09 -0700

    Demonstrating rendering dynamically, basic

--- part1/part1c/src/main.jsx
+++ part1/part1c/src/main.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+let counter = 1;
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App counter={counter} />
+  )
+}
+setInterval(() => {
+  refresh()
+  counter++;
+}, 1000)
+/*
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App counter={counter} />
   </React.StrictMode>,
-)
+) */
