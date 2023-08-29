f8946780c053d2f12edd9acd1f4139e3be5b6c9f
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-08-28 21:28:42 -0700

    component lessons

--- part1/part1c/src/App.jsx
+++ part1/part1c/src/App.jsx
@@ -14,6 +14,8 @@ const Hello = ({name, age}) => {
 
 const Display = ({ counter }) => <div>{counter}</div>
 
+const Display2 = props => <div>{props.value}</div>
+
 const History = (props) => {
   if (props.allClicks.length === 0) {
     return (
@@ -35,14 +37,39 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
+const Button2 = (props) => (
+  <button onclick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
+  const [value, setToValue] = useState(10)
+  return (
+    <div>
+      <Display2 value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+const AppEventHandle = () => {
   const [value, setValue] = useState(10)
 
-  const hello = (who) => {
-    const handler = () => {
-      console.log("hello", who)
-    }
-    return handler
+  const hello = (who) => () => {
+    console.log("hello", who)
+  }
+
+  const setToValue = (newValue) => {
+    console.log("value now", newValue)
+    setValue(newValue)
+  }
+
+  const setToValueComplex = (newValue) => () => {
+    console.log("value now", newValue)
+    setValue(newValue)
   }
 
   const handleClick = () => {
@@ -56,6 +83,9 @@ const App = () => {
       <button onClick={hello('world')}>button</button>
       <button onClick={hello('react')}>button</button>
       <button onClick={hello('function')}>button</button>
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
     </div>
   )
 }
