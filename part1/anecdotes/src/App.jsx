7fd6a836c326f699fb12fd06fdf0e4189d6a3eeb
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-08-29 19:20:15 -0700

    get most voted through array or through dict

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     voteCopyArr[selected] += 1
     setVoteArr(voteCopyArr)
   }
-  
+
   const changeSelect = () => {
     let selectedLocal = Math.floor((Math.random() * anecdotes.length))
     //console.log(selectedLocal)
@@ -47,16 +47,71 @@ const App = () => {
     }
   }
 
+  const displayMostVoted = () => {
+    //const voteCopyDict = {...voteDictionary}
+    let mostVotedKey = 0
+    let mostVotes = 0
+    for(const [key, value] of Object.entries(voteDictionary)) {
+      if (value > mostVotes) {
+        mostVotedKey = key
+        mostVotes = value
+      }
+    }
+    if(mostVotes == 0) {
+      return (
+        <>
+        <div>There's no "most voted" anecdote yet</div>
+        </>
+      )
+    }
+    return (
+      <>
+        <div>{anecdotes[mostVotedKey]}</div>
+        <div>Votes: {mostVotes}</div>
+      </>
+    )
+  }
+
+  const displayMostVotedArr = () => {
+    let mostVotedIndex = -1
+    let mostVotes = -1
+    voteArray.forEach((votes, index) => {
+      if (votes > mostVotes) {
+        mostVotedIndex = index
+        mostVotes = votes
+      }
+    })
+    if(mostVotes == 0) {
+      return (
+        <>
+        <div>There's no "most voted" anecdote yet</div>
+        </>
+      )
+    }
+    return (
+      <>
+        <div>{anecdotes[mostVotedIndex]}</div>
+        <div>Votes: {mostVotes}</div>
+      </>
+    )
+  }
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <button onClick={voteAnecdote}>Vote (Dict)</button>
       <button onClick={voteAnecdoteArr}>Vote (Arr)</button>
       <button onClick={changeSelect}>Random anecdote</button>
       <br />
       <div>Votes (Dict): <span>{displayVote()}</span></div>
       <div>Votes (Array): <span>{voteArray[selected]}</span></div>
-      <hr />
       {anecdotes[selected]}
+      <hr />
+      <h1>Anecdote with most votes (Dict)</h1>
+      <div>{displayMostVoted()}</div>
+      <hr />
+      <h1>Anecdote with most votes (Array)</h1>
+      <div>{displayMostVotedArr()}</div>
     </div>
   )
 }
