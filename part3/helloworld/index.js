c26ad7445fa2ca777f4f849111287b88afd0add0
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-09-04 14:40:39 -0700

    part3 request a string first

--- part3/helloworld/index.js
+++ part3/helloworld/index.js
@@ -1,4 +1,5 @@
-const http = require("http")
+const express = require("express")
+const app = express()
 
 let notes = [
     {
@@ -18,11 +19,25 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "application/json" })
-    response.end(JSON.stringify(notes))
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World!</h1>")
+})
+
+app.get("/api/notes", (request, response) => {
+    response.json(notes)
+})
+
+app.get("/api/notes/:id", (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => {
+        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+        return note.id === id
+    })
+    console.log(note)
+    response.json(note)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on por ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
