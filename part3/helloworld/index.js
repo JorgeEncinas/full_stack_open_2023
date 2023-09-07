6b0172c3a8944cd31739413ef5cd99349ac65116
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-09-06 18:23:55 -0700

    added notes to part3

--- part3/helloworld/index.js
+++ part3/helloworld/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 
 let notes = [
@@ -91,7 +93,7 @@ const unknownEndPoint = (request, response) => {
 }
 app.use(unknownEndPoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
