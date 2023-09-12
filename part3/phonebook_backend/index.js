9f66056f8ae95b7ff0999863a924b184e3af09ab
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-09-11 19:40:09 -0700

    ex. 3.22 done. Used --fix for tabs, for person.js, and for mongo.js

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,20 +1,21 @@
 require("dotenv").config()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
 //const morganmw = morgan("tiny")
 const cors = require("cors")
 morgan.token("post",
-    (request, response) => {
-        let contentString = ""
-        if(request.method == "POST" || request.method == "PUT") {
-            contentString = JSON.stringify(request.body)
-        }
-        return contentString
-})
+	(request) => {
+		let contentString = ""
+		if(request.method === "POST" || request.method === "PUT") {
+			contentString = JSON.stringify(request.body)
+		}
+		return contentString
+	}
+)
 const morganNewTiny = morgan(":method :url :status :res[content-length] - :response-time ms :post")
-const Person = require("./models/person");
+const Person = require("./models/person")
 
 app.use(express.json())
 app.use(morganNewTiny)
@@ -22,123 +23,119 @@ app.use(cors())
 app.use(express.static("dist"))
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-    if(error.name === "CastError") {
-        return response.status(400).send({
-            error:"Your request had the wrong format."
-        })
-    } else if (error.name === "ValidationError") {
-        //console.log(error)
-        return response.status(400).json({
-            error: error.message
-        })
-    }
-    next(error) //Pass it to the default errHandler?
+	console.log(error.message)
+	if(error.name === "CastError") {
+		return response.status(400).send({
+			error:"Your request had the wrong format."
+		})
+	} else if (error.name === "ValidationError") {
+		//console.log(error)
+		return response.status(400).json({
+			error: error.message
+		})
+	}
+	next(error) //Pass it to the default errHandler?
 }
 
 app.get("/api/persons", (request, response) => {
-    Person.find({}).then(persons => {
-        return response.json(persons)
-    })
+	Person.find({}).then(persons => {
+		return response.json(persons)
+	})
 })
 
 app.get("/info", (request, response, next) => {
-    Person.find({}).then(persons => {
-        const info = `
+	Person.find({}).then(persons => {
+		const info = `
         <p>Phonebook has info for ${persons.length} ${persons.length === 1 ? "person" : "people"}</p>
         <p>Request made on: ${Date()}</p>
         `
-        response.set("Content-Type", "text/html")
-        return response.send(info)
-    })
-    .catch(error => {
-        next(error)
-    })
+		response.set("Content-Type", "text/html")
+		return response.send(info)
+	})
+		.catch(error => {
+			next(error)
+		})
 })
 
 app.get("/api/persons/:id", (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(retrievedPerson => {
-            if(retrievedPerson) {
-                return response.json(retrievedPerson)
-            } else {
-                return response.status(404).end()
-            }
-        })
-        .catch(error => {
-            next(error)
-        })
+	Person.findById(request.params.id)
+		.then(retrievedPerson => {
+			if(retrievedPerson) {
+				return response.json(retrievedPerson)
+			} else {
+				return response.status(404).end()
+			}
+		})
+		.catch(error => {
+			next(error)
+		})
 })
 
 app.delete("/api/persons/:id", (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            if(result === null) {
-                response.status(404).end()
-            } else {
-                response.status(204).end()
-            }
-        })
-        .catch(error => next(error))
+	Person.findByIdAndDelete(request.params.id)
+		.then(result => {
+			if(result === null) {
+				response.status(404).end()
+			} else {
+				response.status(204).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
-const generateId = () => {
-    return Math.floor(Math.random()*100000)
-}
-
 app.post("/api/persons", (request, response, next) => {
-    const body = request.body
-    if(!body) {
-        return response.status(400).json({
-            error:"No body."
-        })
-    }
-    if (body.number === undefined || body.name === undefined
+	const body = request.body
+	if(!body) {
+		return response.status(400).json({
+			error:"No body."
+		})
+	}
+	if (body.number === undefined || body.name === undefined
         || body.number === null || body.name === null) {
-        return response.status(400).json({
-            error: "Requires both 'name' and 'number'"
-        })
-    } else {
-        const newPerson = Person({
-            name: body.name,
-            number: body.number
-        })
-        newPerson.save().then(savedPerson => {
-            return response.json(savedPerson)
-        })
-        .catch(error => {
-            next(error)
-        })
-    }
+		return response.status(400).json({
+			error: "Requires both 'name' and 'number'"
+		})
+	} else {
+		const newPerson = Person({
+			name: body.name,
+			number: body.number
+		})
+		newPerson.save().then(savedPerson => {
+			return response.json(savedPerson)
+		})
+			.catch(error => {
+				next(error)
+			})
+	}
 })
 
 app.put("/api/persons/:id", (request, response, next) => {
-    const body = request.body
+	const body = request.body
 
-    if (!body) {
-        response.status(400).json({
-            error: "Person content was missing"
-        })
-    } else {
-        console.log(body)
-        if(body.number) {
-            const editPerson = {
-                //name: body.name,
-                number: body.number
-            }
-            Person.findByIdAndUpdate(request.params.id,
-                editPerson,
-                {new: true, runValidators: true, context: 'query'})
-                .then(updatedPerson => {
-                    response.json(updatedPerson)
-                })
-                .catch(error => next(error))
-        } else {
-            response.status(400).json({
-                error: "Missing attributes"
-            })
-        }
-    }
+	if (!body) {
+		response.status(400).json({
+			error: "Person content was missing"
+		})
+	} else {
+		console.log(body)
+		if(body.number) {
+			const editPerson = {
+				//name: body.name,
+				number: body.number
+			}
+			Person.findByIdAndUpdate(request.params.id,
+				editPerson,
+				{ new: true, runValidators: true, context: "query" })
+				.then(updatedPerson => {
+					response.json(updatedPerson)
+				})
+				.catch(error => next(error))
+		} else {
+			response.status(400).json({
+				error: "Missing attributes"
+			})
+		}
+	}
 })
 
 app.use(errorHandler)
--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -3,43 +3,43 @@ const url = process.env.MONGODB_URI_PHONEBOOK
 
 mongoose.set("strictQuery", false)
 mongoose.connect(url)
-    .then(response => {
-        console.log("Connected to MongoDB")
-    })
-    .catch(error => {
-        console.log("Error connecting to MongoDB", error.message)
-    })
+	.then(() => {
+		console.log("Connected to MongoDB")
+	})
+	.catch(error => {
+		console.log("Error connecting to MongoDB", error.message)
+	})
 
 const personSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 3
-    },
-    number: {
-        type: String,
-        required: true,
-        minLength: 8,
-        validate: {
-            validator: function(v) {
-                //I had some trouble here, bc it didn't respect the length threshold {2,3}
-                //Checked this, but it didn't help all that much:
-                //https://stackoverflow.com/questions/5034948/regex-for-a-string-of-length-0-2
-                //https://stackoverflow.com/questions/4824942/regex-to-match-digits-of-specific-length
-                //https://www.regular-expressions.info/anchors.html --This one seems useful to go back to.
-                return /^[0-9]{2,3}-\d/.test(v);
-            },
-            message: props => `${props.value} must be in xxx-yyyy format. Have 2-3 numbers before the dash.`
-        }
-    }
+	name: {
+		type: String,
+		required: true,
+		minLength: 3
+	},
+	number: {
+		type: String,
+		required: true,
+		minLength: 8,
+		validate: {
+			validator: function(v) {
+				//I had some trouble here, bc it didn't respect the length threshold {2,3}
+				//Checked this, but it didn't help all that much:
+				//https://stackoverflow.com/questions/5034948/regex-for-a-string-of-length-0-2
+				//https://stackoverflow.com/questions/4824942/regex-to-match-digits-of-specific-length
+				//https://www.regular-expressions.info/anchors.html --This one seems useful to go back to.
+				return /^[0-9]{2,3}-\d/.test(v)
+			},
+			message: props => `${props.value} must be in xxx-yyyy format. Have 2-3 numbers before the dash.`
+		}
+	}
 })
 
 personSchema.set("toJSON", {
-    transform: (document, returnedObj) => {
-        returnedObj.id = returnedObj._id.toString() //bc it's originally an Object.
-        delete returnedObj._id
-        delete returnedObj.__v
-    }
+	transform: (document, returnedObj) => {
+		returnedObj.id = returnedObj._id.toString() //bc it's originally an Object.
+		delete returnedObj._id
+		delete returnedObj.__v
+	}
 })
 
 const Person = mongoose.model("Person", personSchema)
