4d70187ec5fdbd1af9a4b8add4fab983614e1b70
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-09-12 21:35:42 -0700

    ex 4.4 done

--- part4/blog_backend/utils/list_helper.js
+++ part4/blog_backend/utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
-    return 1
+	return 1
+}
+
+const totalLikes = (blogs) => {
+	console.log(blogs.length)
+	if (blogs.length === 0) {
+		return 0
+	} else {
+		return blogs.reduce((accumulator, blog) => {
+			let likes = blog.likes ? blog.likes : 0
+			return accumulator + likes
+		}, 0)
+	}
 }
 
 module.exports = {
-    dummy
+	dummy,
+	totalLikes
 }
