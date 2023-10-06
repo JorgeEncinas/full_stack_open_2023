e100127df0ec690912916b6b249d1399b03c3018
Jorge Encinas <jorgecarlosencinas@gmail.com>
2023-10-05 19:26:41 -0700

    finished await section

--- part4/notes_backend/controllers/notes.js
+++ part4/notes_backend/controllers/notes.js
@@ -6,30 +6,29 @@ notesRouter.get("/", async (request, response) => {
     response.json(notes)
 })
 
-notesRouter.get("/:id", (request, response, next) => {
-    Note.findById(request.params.id).then(retrievedNote => {
-        if(retrievedNote) {
-            response.json(retrievedNote)
+notesRouter.get("/:id", async (request, response, next) => {
+    try {
+        const note = await Note.findById(request.params.id)
+        if (note) {
+            response.json(note)
         } else {
             response.status(404).end()
         }
-    })
-    .catch(error => next(error))
+    } catch(exception) {
+        next(exception)
+    }
 })
 
-notesRouter.delete("/:id", (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
-        .then(result => {
-            if(result === null) {
-                response.status(404).end()
-            } else {
-                response.status(204).end()
-            }
-        })
-        .catch(error => next(error))
+notesRouter.delete("/:id", async (request, response, next) => {
+    try {
+        await Note.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch (exception) {
+        next(exception)
+    }
 })
 
-notesRouter.post("/", (request, response, next) => {
+notesRouter.post("/", async (request, response, next) => {
     const body = request.body
     if (body.content === undefined) { //undefined and null are both falsey
         return response.status(400).json({
@@ -41,10 +40,12 @@ notesRouter.post("/", (request, response, next) => {
         important: body.important || false
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
-    .catch(error => next(error))
+    try {
+        const savedNote = await note.save()
+        response.status(201).json(savedNote)
+    } catch(exception) {
+        next(exception)
+    }
 })
 
 notesRouter.put("/:id", (request, response, next) => {
